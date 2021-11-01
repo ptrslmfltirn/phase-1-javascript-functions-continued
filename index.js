d2dbaf0865b11c29a2edb0996847d3bcd84fc2a7
Peter Lamson <ptrlmsn@gmail.com>
2021-11-01 17:02:00 -0400

    commit tree 87a44222a89aaa689f0e12074b3103c24b0e84ac

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function saturdayFun (activity) {
+    if (typeof activity === 'string') {
+    return `This Saturday, I want to ${activity}!`
+    }
+    else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+function mondayWork (job) {
+    if (typeof job === 'string') {
+    return `This Monday, I will ${job}.`
+    }
+    else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+function wrapAdjective (flair = "*") {
+    return function (adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
